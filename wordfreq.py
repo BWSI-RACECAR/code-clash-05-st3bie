42be042c3d3bef6a0749d17b33a30cc6629c9728
Steven <64932989+st3bie@users.noreply.github.com>
2022-07-15 07:23:39 -0700

    Update wordfreq.py

--- wordfreq.py
+++ wordfreq.py
@@ -27,16 +27,7 @@ Input: “How high can his white dog hike”;   Output: 4
 class Solution:
     def hi_finder(self,hi_string):
         
-        counter = 0
-        hi_string = hi_string.lower()
-
-        while ("hi" in hi_string):
-            index = hi_string.index("hi")
-            hi_string = hi_string[0 : index - 1] + hi_string[index + 1:]
-            counter += 1
-        
-        # TODO: Write code below to return an int with the solution to the prompt
-        return counter
+        return hi_string.lower().count("hi")
 
 def main():
     string1 = input()
